d8e802f8137feae3f535ad2f628cff2f52199ef3
Jamie Atyeo <jamie.atyeo@hootsuite.com>
2025-01-13 18:41:47 -0800

    implement human choice, round, and game.

--- script.js
+++ script.js
@@ -9,3 +9,44 @@ let getComputerChoice = () => {
             return 'scissors';
     }
 };
+
+let getHumanChoice = () => {
+    let humanChoice = prompt('Enter your choice: rock, paper, or scissors');
+    if (humanChoice === 'rock' || humanChoice === 'paper' || humanChoice === 'scissors') {
+        return humanChoice;
+    } else {
+        console.log('Invalid choice!');
+    }
+};
+
+let humanScore = 0;
+let computerScore = 0;
+
+let playRound = (humanChoice, computerChoice) => {
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+    if (humanChoice === computerChoice) {
+        console.log('It\'s a tie!');
+    } else if (humanChoice === 'rock' && computerChoice === 'scissors' || humanChoice === 'paper' && computerChoice === 'rock' || humanChoice === 'scissors' && computerChoice === 'paper') {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+    } else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    }
+}
+
+let playGame = () => {
+    for (let i = 0; i < 5; i++) {
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+    if (humanScore > computerScore) {
+        console.log('You win the game!');
+    } else if (humanScore < computerScore) {
+        console.log('You lose the game!');
+    } else {
+        console.log('It\'s a tie!');
+    }
+}
+
+playGame();
